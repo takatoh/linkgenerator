8e806e457b50134dd7ab60c3d95bdf7e3eb5c0a1
takatoh <takatoh.m@gmail.com>
2012-02-09 17:34:00 +0900

    genlinks.rb: LinkGenerator class is new.

--- genlinks.rb
+++ genlinks.rb
@@ -1,12 +1,25 @@
-posts = File.readlines(ARGV.shift).map{|l| l.chomp}
 
-puts "<html>"
-puts "  <body>"
-puts "    <ol>"
-posts.each do |url|
-  puts "      <li><a href=\"#{url}\">#{url}</a></li>"
-end
-puts "    </ol>"
-puts "  </body>"
-puts "</html>"
+class LinkGenerator
+
+  def initialize(file)
+    @links = File.readlines(file).map{|l| l.chomp}
+  end
+
+  def to_html
+    puts "<html>"
+    puts "  <body>"
+    puts "    <ol>"
+    @links.each do |url|
+      puts "      <li><a href=\"#{url}\">#{url}</a></li>"
+    end
+    puts "    </ol>"
+    puts "  </body>"
+    puts "</html>"
+  end
+
+end   # of class LinkGenerator
+
+
+file = ARGV.shift
+LinkGenerator.new(file).to_html
 
