2e13567630012b75191284d4eb1827b861242605
takatoh <takatoh.m@gmail.com>
2012-02-10 14:36:31 +0900

    genlinks.rb: --help option.

--- genlinks.rb
+++ genlinks.rb
@@ -1,3 +1,7 @@
+#! ruby
+
+require 'optparse'
+
 
 class LinkGenerator
 
@@ -20,6 +24,18 @@ class LinkGenerator
   end
 
 end   # of class LinkGenerator
+
+
+
+opt = OptionParser.new
+opt.banner = <<EOB
+Usage: #{opt.program_name} [options] <file>
+EOB
+opt.on_tail('-h', '--help', 'Show this message.'){|v|
+  print opt
+  exit
+}
+opt.parse!
 
 
 print LinkGenerator.new(ARGV.shift).to_html
