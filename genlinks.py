67e752ae5d28e576b9518687d19a21207cef60f8
takatoh <takatoh.m@gmail.com>
2013-11-27 21:59:49 +0900

    genlinsk.py: v0.1.0. Support input from csv file.

--- genlinks.py
+++ genlinks.py
@@ -1,15 +1,23 @@
 import sys
+import csv
 from jinja2 import Environment, DictLoader
 import argparse
 
 
-script_version = 'v0.0.1'
+script_version = 'v0.1.0'
+
+
+class Link:
+    def __init__(self, url, sitename):
+        self.url = url
+        self.sitename = sitename
+
 
 templates = {'index.html': """<html>
   <body>
     <ul>
-      {% for url in urls %}
-        <li><a href="{{ url }}">{{ url }}</a></li>
+      {% for link in links %}
+        <li><a href="{{ link.url }}">{{ link.sitename }}</a></li>
       {% endfor %}
     </ul>
   </body>
@@ -33,15 +41,21 @@ if len(sys.argv) == 1:
     sys.exit()
 
 filename = sys.argv[1]
-f = open(filename, 'r')
-urls = []
-for line in f:
-    urls.append(line.rstrip('\n'))
-f.close
+csvfile = open(filename, 'r')
+reader = csv.reader(csvfile)
+links = []
+for row in reader:
+    url = row[0]
+    try:
+        sitename = row[1]
+    except IndexError:
+        sitename = url
+    links.append(Link(url, sitename))
+csvfile.close
 
 env = Environment(loader=DictLoader(templates))
 tmpl = env.get_template('index.html')
-html = tmpl.render(urls=urls)
+html = tmpl.render(links=links)
 
 print html
 
